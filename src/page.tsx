b96d321aa46b2d1ce342e3366c78ac2fe6bf114f
David Dossou <david.dossou@sky.uk>
2024-02-03 23:49:06 +0000

    attempt 2

--- src/page.tsx
+++ src/page.tsx
@@ -13,44 +13,41 @@ export default function Page() {
   const getNoButtonText = () => {
     const phrases = [
       "No",
-      "Are you sure?",
-      "Really sure?",
-      "Think again!",
-      "Last chance!",
-      "Surely not?",
-      "You might regret this!",
-      "Give it another thought!",
+      "Are you sure Mel?",
+      "Babe are you really sure?",
+      "Think again babe",
+      "I won't ask again smh",
+      "Surely not Mel?",
+      "I know you're just joking anyways",
       "Are you absolutely certain?",
-      "This could be a mistake!",
-      "Have a heart!",
-      "Don't be so cold!",
+      "This is deffo a mistake!",
+      "So you don't love me?",
       "Change of heart?",
       "Wouldn't you reconsider?",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
+      "Is that your final answer Mel?",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen -mt-16">
+    <div className="question">
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" alt="pic" />
-          <div className="text-4xl font-bold my-4">Ok yay!!!</div>
+          <div className="text-4xl font-bold my-4">I definitely knew you'd say yes babe! I love you</div>
         </>
       ) : (
         <>
           <img
-            className="h-[200px]"
+            className="bear"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
             alt="pic"
           />
-          <h1 className="text-4xl my-4">Will you be my Valentine?</h1>
+          <h1 className="text">Will you be my Valentine Melissa?</h1>
           <div>
             <button
-              className={`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-4`}
+              className={`accept`}
               style={{ fontSize: yesButtonSize }}
               onClick={() => setYesPressed(true)}
             >
@@ -58,7 +55,7 @@ export default function Page() {
             </button>
             <button
               onClick={handleNoClick}
-              className=" bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+              className="decline"
             >
               {noCount === 0 ? "No" : getNoButtonText()}
             </button>
